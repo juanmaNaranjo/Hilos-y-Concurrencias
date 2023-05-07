f5401c1776f85f3408b58cd8e5d84cd643540ea6
Juanma <juanmanuel.mejia@utp.edu.co>
2023-05-07 15:41:47 -0500

    cambio de tiempo

---
+++ practices/ExecutorService/src/com/example/client/NetworkClientCallable.java
@@ -0,0 +1,23 @@
+package com.example.client;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+
+public class NetworkClientCallable implements Callable<RequestResponse> {
+    RequestResponse lookup;
+    public NetworkClientCallable(RequestResponse lookup) {
+        this.lookup = lookup;
+    }
+    @Override
+    public RequestResponse call() {
+        try (Socket socket = new Socket(lookup.host, lookup.port);
+             Scanner scanner = new Scanner(socket.getInputStream())) {
+            this.lookup.response = scanner.next();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return this.lookup;
+    }
+}
